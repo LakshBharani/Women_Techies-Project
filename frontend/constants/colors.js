d55e6843ae93a1cc40636d756b5e8fcdf5ea56fb
LakshBharani <laksh.bharani@gmail.com>
2024-03-09 11:17:12 +0530

    Co-authored-by: Soham-Maha <Soham-Maha@users.noreply.github.com> Co-authored-by: Pravenraam Shankar <praventhegenius@users.noreply.github.com>

---
+++ frontend/constants/colors.js
@@ -0,0 +1,11 @@
+// https://coolors.co/ff7477-e69597-ceb5b7-b5d6d6-9cf6f6
+
+export const Colors = {
+  lightMode: {
+    primary500: "#ff6b6e",
+    primary300: "#e69597",
+    primary200: "#ceb5b7",
+    secondary: "#b5d6d6",
+    secondaryAccent: "#9cf6f6",
+  },
+};
---
+++ frontend/screens/Auth/SigninScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const SigninScreen = () => {
+  return (
+    <View>
+      <Text>SigninScreen</Text>
+    </View>
+  );
+};
+
+export default SigninScreen;
+
+const styles = StyleSheet.create({});
