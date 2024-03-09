c9056fefec1d1655d4cdd348afd2923291b9a1e5
Soham-Maha <soham.mahapatra2023@gmail.com>
2024-03-09 22:43:57 +0530

    registration done

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,51 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
-const dbconnection = require("./config/DBconnection.js");
+const mongoUrl =
+  "mongodb+srv://sohammaha15:sm123456@cluster0.fdtfvyj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-// const userRoutes = require("./route/users/usersRoutes.js");
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send({staus:"Started"});
+  res.send("Hello World");
+});
+require("./Users");
+const User = mongoose.model("UserInfo");
+
+app.post("/register", async (req, res) => {
+  const { name, email, mobile, password } = req.body;
+  try {
+    const oldUser = await User.findOne({ email: email });
+
+    if (oldUser) {
+      throw new Error("User already exists");
+    }
+
+    const createdUser = await User.create({
+      name: name,
+      email: email,
+      mobile: mobile,
+      password: password,
+    });
+    res.status(200).json({ success: true, createdUser });
+  } catch (error) {
+    res.status(401).json({
+      success: false,
+      message: error.message,
+    });
+  }
 });
 
-dbconnection();
-
-app.listen(5000, () => {
+app.listen(5001, () => {
   console.log("Server is running ");
 });
-
-// app.use('/api/users',userRoutes);
-
--- backend/route/users/usersRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { registerUser } = require("../../controllers/Users/userController");
-
-const route = express.Router();
-
-route.post('/register',registerUser);
-
-module.exports = route;
